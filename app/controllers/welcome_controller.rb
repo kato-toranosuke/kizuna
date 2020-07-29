facef1a495ff24c460db78212522583c8a9a243c
tabenokosi <goodbyetuesday3@gmail.com>
2020-07-29 11:01:20 +0900

    17

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,14 +2,84 @@ class WelcomeController < ApplicationController
   def index
     @rests=Nanao.all
   end
-  def search
-    # @rests=Nanao.none
-    if params[:name].present?
-      @rests = Nanao.where('name LIKE ?', "%#{params[:name]}%")
-    else
-      @rests = Nanao.none
-    end
 
+  def search
+    if params[:name].present?  
+ 
+      if params[:touchless].present? 
+  
+          if params[:mask].present?
+  
+              if params[:takeout].present?  #name,touchless,mask,takeout
+               @rests = Nanao.where('name LIKE ?', "%#{params[:name]}%").where(touchless: true).where(mask: true).where(takeout: true)
+              else  #name,touchless,mask
+               @rests = Nanao.where('name LIKE ?', "%#{params[:name]}%").where(touchless: true).where(mask: true) 
+              end
+  
+          else
+              if params[:takeout].present?  #name,touchless,takeout
+               @rests = Nanao.where('name LIKE ?', "%#{params[:name]}%").where(touchless: true).where(takeout: true)
+              else  #name,touchless
+               @rests = Nanao.where('name LIKE ?', "%#{params[:name]}%").where(touchless: true)
+              end
+          end
+  
+      else
+          if params[:mask].present?
+             
+              if params[:takeout].present?  #name,mask,takeout
+               @rests = Nanao.where('name LIKE ?', "%#{params[:name]}%").where(mask: true).where(takeout: true) 
+              else  #name,mask
+               @rests = Nanao.where('name LIKE ?', "%#{params[:name]}%").where(mask: true) 
+              end
+  
+          else
+              if params[:takeout].present?  #name,takeout
+               @rests = Nanao.where('name LIKE ?', "%#{params[:name]}%").where(takeout: true)
+              else  #name
+               @rests = Nanao.where('name LIKE ?', "%#{params[:name]}%")
+              end
+          end
+      end
+      
+    else
+
+      if params[:touchless].present?
+   
+           if params[:mask].present?
+   
+               if params[:takeout].present?  #touchless,mask,takeout
+                @rests = Nanao.where(touchless: true).where(mask: true).where(takeout: true) 
+               else  #touchless,mask
+                @rests = Nanao.where(touchless: true).where(mask: true) 
+               end
+   
+           else
+               if params[:takeout].present?  #touchless,takeout
+                 @rests = Nanao.where(touchless: true).where(takeout: true) 
+               else  #touchless
+                 @rests = Nanao.where(touchless: true) 
+               end
+           end
+   
+       else
+           if params[:mask].present?
+   
+               if params[:takeout].present?  #mask,takeout
+                 @rests = Nanao.where(mask: true).where(takeout: true)  
+               else  #mask
+                 @rests = Nanao.where(mask: true)
+               end
+   
+           else
+               if params[:takeout].present?  #takeout
+                 @rests = Nanao.where(takeout: true) 
+               else  #none
+                 @rests = Nanao.none 
+               end
+           end
+       end
+    end
 
   end
 
