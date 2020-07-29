d0c932b82ffc41c5d1040bf5e79c778738b435d4
tabenokosi <goodbyetuesday3@gmail.com>
2020-07-29 12:01:27 +0900

    19

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -89,27 +89,27 @@ class WelcomeController < ApplicationController
   end
 
   def result
-    Nanao.create(name: params[:name],
-            address: params[:address],
-            alcohol: params[:alcohol],
-            mask: params[:mask],
-            time: params[:time],
-            takeout: params[:takeout],
-            touchless: params[:touchless],
-            card: params[:card],
-            linepay: params[:linepay],
-            paypay: params[:paypay],
-            payid: params[:payid],
-            quicpay: params[:quicpay],
-            seat: params[:seat],
-            contact: params[:contact],
-            toilet: params[:toilet],
-            airing: params[:airing],
-            outdoor: params[:outdoor],
-            temp: params[:temp],
-            atom: params[:atom],
-            plate: params[:plate],
-            rate: params[:rate])
+    # Nanao.create(name: params[:name],
+    #         address: params[:address],
+    #         alcohol: params[:alcohol],
+    #         mask: params[:mask],
+    #         time: params[:time],
+    #         takeout: params[:takeout],
+    #         touchless: params[:touchless],
+    #         card: params[:card],
+    #         linepay: params[:linepay],
+    #         paypay: params[:paypay],
+    #         payid: params[:payid],
+    #         quicpay: params[:quicpay],
+    #         seat: params[:seat],
+    #         contact: params[:contact],
+    #         toilet: params[:toilet],
+    #         airing: params[:airing],
+    #         outdoor: params[:outdoor],
+    #         temp: params[:temp],
+    #         atom: params[:atom],
+    #         plate: params[:plate],
+    #         rate: params[:rate])
   end
 
 end
